5ac17e10efc33a815a80816151ed8b7324139eb6
Matheus Cesar Reis Pereira <11695042646@dc01.ifs.ifsuldeminas.edu.br>
2022-07-07 07:13:11 -0300

    Projeto_Final

---
+++ projeto_final/view/categoria/listagem.php
@@ -0,0 +1,26 @@
+<!-- Conteudo da pagina -->
+<div class="container">
+    <h1 class="mt-2">Lista de Categorias</h1>
+    <hr>
+
+    <a href="<?= base_url() . "?c=categoria&m=add" ?>" class="btn btn-success">Inserir Categoria</a>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($categorias as $categoria) : ?>
+                <tr>
+                    <td><?= $categoria['nome'] ?></td>
+                    <td>
+                        <a href="<?php echo base_url() . "?c=categoria&m=excluir&id=" . $categoria['idcategoria']; ?>" class="btn btn-danger" title="Excluir"><i class="fa-solid fa-trash-can"></i></a>
+                        <a href="<?php echo base_url() . "?c=categoria&m=editar&id=" . $categoria['idcategoria']; ?>" class="btn btn-primary" title="Atualizar"><i class="fa-solid fa-pencil"></i></a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
---
+++ projeto_final/view/template/conteudo.php
@@ -0,0 +1,5 @@
+<!--Conteudo da Página-->
+<div class="container mt-2">
+    <h1>Conteudo</h1>
+    <hr>
+</div>
